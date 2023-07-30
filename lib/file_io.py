928fd87173c40bce14760ce674c4c6a948755548
Pawan Kumar <pawan.3940@gmail.com>
2023-07-29 18:51:51 -0700

    complete lab

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,11 @@
-def write_file(file_name, file_content):
-    pass
+def write_file(file_name='lib/logfile', file_content='Log 1: 5 bananas added'):
+    with open(f"{file_name}.txt", 'w', encoding="utf-8") as file:
+        file.write(file_content)
 
-def append_file(file_name, append_content):
-    pass
+def append_file(file_name='lib/logfile', append_content="Log 2: 3 bananas subtracted"):
+    with open(f"{file_name}.txt", 'a', encoding="utf-8") as file:
+        file.write(append_content)
 
-def read_file(file_name):
-    pass
+def read_file(file_name='lib/logfile'):
+    with open(f"{file_name}.txt", encoding='utf-8') as file:
+        return file.read()
